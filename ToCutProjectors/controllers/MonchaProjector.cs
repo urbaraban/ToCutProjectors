7735d96446430d78eff940e6918be7a68e093ad1
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2023-04-26 13:04:15 +0700

    mapping

---
+++ ToCutProjectors/controllers/MonchaProjector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToCutProjectors.controllers
+{
+    public class MonchaProjector : LProjector
+    {
+        public static int TCPBeginnerPort => 9764;
+        public static int UDPBeginnerPort => 9765;
+    }
+}
