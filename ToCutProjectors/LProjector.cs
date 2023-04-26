06272a778e6e4cc0beee2df20c2d209a5e2cf650
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2023-04-26 12:41:23 +0700

    Добавьте файлы проекта.

---
+++ ToCutProjectors/LProjector.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace ToCutProjectors
+{
+    public abstract class LProjector
+    {
+        public virtual IPAddress IPAddress { get; set; } = new IPAddress(new byte[] { 127, 0, 0, 1 });
+
+        public virtual bool IsConnected { get; } = true;
+
+        public virtual bool IsOn { get; set; } = true;
+
+        public virtual double HeightResolution { get; set; } = 1000;
+        public virtual double WidthResolutuon { get; set; } = 1000;
+
+    }
+
+    public enum DeviceType : int
+    {
+        Virtual = 0,
+        MonchaNET = 1,
+        VLT = 2,
+        Pangolin = 3,
+    }
+}
---
+++ ToCutProjectorsTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToCutProjectorsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
